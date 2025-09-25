54ca0c6ddb8c39adb470bd4b9056a19ab68df08a
MellloJ <jdesousamadeira@gmail.com>
2025-09-25 08:13:03 -0300

    Implementacao do filtro laplaciano

--- PI-6-filtro-media.py
+++ PI-6-filtro-media.py
@@ -6,29 +6,23 @@ def filtroMedia(matriz, tamanhoJanela=3):
     colunas = len(matriz[0])
     offset = tamanhoJanela // 2
 
-    # Criar matriz nova para armazenar o resultado
+    # Cria uma matriz nova para armazenar o resultado
     matrizNova = [[0 for _ in range(colunas)] for _ in range(linhas)]
 
-    # Percorrer cada pixel da matriz original
     for i in range(linhas):
         for j in range(colunas):
             soma = 0
             cont = 0
 
-            # Percorrer a vizinhança
+            # Percorre a vizinhança
             for x in range(-offset, offset+1):
                 for y in range(-offset, offset+1):
                     linhaViz = i + x
                     colunaViz = j + y
 
-                    # Tratamento de borda por duplicação
-                    if linhaViz < 0:
-                        linhaViz = 0
-                    elif linhaViz >= linhas:
+                    if linhaViz >= linhas:
                         linhaViz = linhas - 1
-                    if colunaViz < 0:
-                        colunaViz = 0
-                    elif colunaViz >= colunas:
+                    if colunaViz >= colunas:
                         colunaViz = colunas - 1
 
                     soma += int(matriz[linhaViz][colunaViz])
@@ -42,7 +36,6 @@ def filtroMedia(matriz, tamanhoJanela=3):
 def main():
     matrizCinza = extrairMatrizCinza()
 
-    print("Aplicando Filtro da Média...")
     matrizFiltrada = filtroMedia(matrizCinza, tamanhoJanela=3)
 
     salvarImagem(np.array(matrizFiltrada, dtype=np.uint8), "imagem_filtro_media.png")
---
+++ PI-7-filtro-laplaciano.py
@@ -0,0 +1,103 @@
+from commons import extrairMatrizCinza, salvarImagem
+import numpy as np
+
+laplacian_masks = {
+    "a": [[0, -1,  0],
+          [-1,  4, -1],
+          [0, -1,  0]],
+    
+    "b": [[-1, -1, -1],
+          [-1,  8, -1],
+          [-1, -1, -1]],
+    
+    "c": [[0,  1,  0],
+          [1, -4,  1],
+          [0,  1,  0]],
+    
+    "d": [[1,  1,  1],
+          [1, -8,  1],
+          [1,  1,  1]]
+}
+
+def duplicarBordas(matriz):
+    linhas = len(matriz)
+    colunas = len(matriz[0])
+
+    novaMatriz = [[0 for _ in range(colunas + 1)] for _ in range(linhas + 1)]
+
+    for i in range(linhas):
+        for j in range(colunas):
+            novaMatriz[i][j] = matriz[i][j]
+
+    for i in range(linhas):
+        novaMatriz[i][colunas] = matriz[i][colunas-1]
+
+    for j in range(colunas):
+        novaMatriz[linhas][j] = matriz[linhas-1][j]
+
+    novaMatriz[linhas][colunas] = matriz[linhas-1][colunas-1]
+
+    return novaMatriz
+
+
+def aplicarMascaraLaplaciano(matriz, mascara):
+    linhas = len(matriz)
+    colunas = len(matriz[0])
+
+    # Trata as bordas
+    matrizExpandida = duplicarBordas(matriz)
+
+    matrizNova = [[0 for _ in range(colunas)] for _ in range(linhas)]
+
+    for i in range(linhas):
+        for j in range(colunas):
+            soma = 0
+            for mi in range(3):
+                for mj in range(3):
+                    soma += matrizExpandida[i+mi][j+mj] * mascara[mi][mj]
+
+            # Normalização para [0, 255]
+            if soma < 0:
+                soma = 0
+            elif soma > 255:
+                soma = 255
+
+            matrizNova[i][j] = soma
+
+    return matrizNova
+
+def main():
+    matrizCinza = extrairMatrizCinza()
+
+    print("Selecione a máscara Laplaciana:")
+    print("1 - Máscara A")
+    print("2 - Máscara B")
+    print("3 - Máscara C")
+    print("4 - Máscara D")
+    opcao = int(input())
+
+    mascara = None
+    nomeMascara = ""
+    if opcao == 1:
+        mascara = laplacian_masks["a"]
+        nomeMascara = "a"
+    elif opcao == 2:
+        mascara = laplacian_masks["b"]
+        nomeMascara = "b"
+    elif opcao == 3:
+        mascara = laplacian_masks["c"]
+        nomeMascara = "c"
+    elif opcao == 4:
+        mascara = laplacian_masks["d"]
+        nomeMascara = "d"
+    else:
+        print("Opção inválida.")
+        return
+
+    matrizFiltrada = aplicarMascaraLaplaciano(matrizCinza, mascara)
+
+    matrizFiltrada = np.array(matrizFiltrada, dtype=np.uint8)
+    salvarImagem(matrizFiltrada, f"laplaciano_{nomeMascara}.png")
+
+if __name__ == "__main__":
+    main()
